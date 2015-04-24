8f12e63e0fb1efcc27829b528aba9461b076876c
Hwasoo Lee <hlee@hvhc.com>
2015-04-24 16:33:32 -0400

    Changed the version numbering.

--- FileNameSerializer/Properties/AssemblyInfo.cs
+++ FileNameSerializer/Properties/AssemblyInfo.cs
@@ -2,17 +2,21 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+using CommandLine;
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FileNameSerializer")]
+[assembly: AssemblyTitle("FileNameSerializer Tool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FileNameSerializer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright (C) 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyLicense("MIT License")]
+[assembly: AssemblyUsage("FileNameSerializer -d:c:\\temp : it will search files specified extension under the folder.")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+
