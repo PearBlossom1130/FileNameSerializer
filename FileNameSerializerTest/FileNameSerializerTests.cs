723f3d34451b49600f58bb77925b6e6120258523
Hwasoo Lee <hlee@hvhc.com>
2015-04-24 11:31:30 -0400

    Sorted the converted files by creation time ascending.

--- FileNameSerializerTest/FileNameSerializerTests.cs
+++ FileNameSerializerTest/FileNameSerializerTests.cs
@@ -11,9 +11,9 @@ namespace FileNameSerializerTest
     public class FileNameSerializerTests
     {
         private readonly static string DirWithASubDirMp4Files = Directory.GetCurrentDirectory() + "\\DirWithASubDirMp4Files";
-        private readonly static string DirWithTwoSubDirsMp4FilesRoot = Directory.GetCurrentDirectory() + "\\DirWithTwoSubDirsMp4FilesRoot";
-        private readonly static string DirWithTwoSubDirsMp4FilesSub1 = DirWithTwoSubDirsMp4FilesRoot + "\\DirWithTwoSubDirsMp4FilesSub1";
-        private readonly static string DirWithTwoSubDirsMp4FilesSub2 = DirWithTwoSubDirsMp4FilesRoot + "\\DirWithTwoSubDirsMp4FilesSub2";
+        private readonly static string DirWithTwoSubDirsMp4Files = Directory.GetCurrentDirectory() + "\\DirWithTwoSubDirsMp4FilesRoot";
+        private readonly static string DirWithTwoSubDirsMp4FilesSub1 = DirWithTwoSubDirsMp4Files + "\\DirWithTwoSubDirsMp4FilesSub1";
+        private readonly static string DirWithTwoSubDirsMp4FilesSub2 = DirWithTwoSubDirsMp4Files + "\\DirWithTwoSubDirsMp4FilesSub2";
 
         private const string FIRST_FILE_TIME = "4/23/2015 6:26:11.209 PM";
         private const string SECOND_FILE_TIME = "4/23/2015 6:27:45.837 PM";
@@ -36,17 +36,17 @@ namespace FileNameSerializerTest
         {
             EnvironmentWorker.GetAllSubDirectories(DirWithASubDirMp4Files);
             EnvironmentWorker.EnqueueDirectories();
-            
+
             string directory = null;
             EnvironmentWorker.TargetDirectories.TryPeek(out directory);
 
             var fns = new FileNameSerializer();
-
             fns.ChangeFileName();
-        
+
             var convertedFiles = Directory.GetFiles(directory);
             var loop = 0;
-            foreach (var fileInfo in convertedFiles.Select(f => new FileInfo(f)))
+
+            foreach (var fileInfo in convertedFiles.OrderBy(f => (new FileInfo(f)).CreationTimeUtc).Select(f => new FileInfo(f)))
             {
                 Assert.AreEqual(fileInfo.CreationTimeUtc, _fileDateTimeStamps[loop++]);
             }
@@ -55,7 +55,7 @@ namespace FileNameSerializerTest
         [TestMethod]
         public void ShouldMatchTimeStampsFromTwoSubDirsContaingMp4Files()
         {
-            EnvironmentWorker.GetAllSubDirectories(DirWithTwoSubDirsMp4FilesRoot);
+            EnvironmentWorker.GetAllSubDirectories(DirWithTwoSubDirsMp4Files);
             EnvironmentWorker.EnqueueDirectories();
 
             Assert.AreEqual(2, EnvironmentWorker.TargetDirectories.Count);
